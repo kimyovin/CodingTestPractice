6f2b17025395588ec1f59f5d653f9d3765570d9a
kimyovin <35512625+kimyovin@users.noreply.github.com>
2021-03-26 16:12:22 +0900

    Update 약수의 합.py
    Add code ‘with help’

--- Programmers/약수의 합.py
+++ Programmers/약수의 합.py
@@ -1,3 +1,4 @@
+#myself
 def solution(n):
   answer =0
   for i in range(1, n+1):
@@ -5,3 +6,13 @@ def solution(n):
       answer += i
       
   return answer
+
+-------------------------------------------------------------
+#with help
+def solution(n):
+    answer = n
+    for i in range(1, n//2+1):
+        if n%i == 0:   
+            answer += i
+    return answer
+-------------------------------------------------------------
