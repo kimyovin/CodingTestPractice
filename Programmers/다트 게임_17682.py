e1c9c47957f28b27436bad0ef1fbe9e27c3ec197
kimyovin <35512625+kimyovin@users.noreply.github.com>
2021-03-31 13:42:17 +0900

    Update 다트 게임_17682.py
    Add other solution and my study point

--- Programmers/다트 게임_17682.py
+++ Programmers/다트 게임_17682.py
@@ -21,4 +21,32 @@ def solution(dartResult):
         print(count, num, answer)
     return answer
 
-# https://programmers.co.kr/learn/courses/30/lessons/17682
+#----------------------------------------------------------------
+#####with other solution1 - 정규표현식에 ()로 그룹화를 하여 튜플화할 수 있다.
+
+import re
+def solution1(dartResult):
+    sqt = ['S', 'D', 'T']
+    option = {'#': -1, '*': 2, '': 1}
+
+    mark = re.findall('(\d+)([SDT])([*#]?)', dartResult)
+    for i in range(len(mark)):
+        print(mark)
+        if mark[i][2] == '*' and i > 0:
+            mark[i-1] *= 2  # Tuple로 이뤄진 원소가 숫자로 된다 ex) ('1', 'S', '') => 1
+        
+        mark[i] = int(mark[i][0]) ** (sqt.index(mark[i][1])+1) * option[mark[i][2]]
+    answer = sum(mark)
+    return answer
+
+'''
+1. findall에서 정규표현식에 ()로 그룹화를 시키면
+    튜플화가 된다.
+    mark = re.findall('([0-9]+)([S|D|T])([*]?#?)', dartResult)
+    1D2S3T*
+    => [('1', 'D', ''), ('2', 'S', ''), ('3', 'T', '*')]
+2. 리스트에서 원소의 형을 바꿔도 된다. => 한정짓지 말자
+    ex) Tuple => int
+   
+https://programmers.co.kr/learn/courses/30/lessons/17682
+'''
