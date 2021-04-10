6f5d936f4bc7c9e1eb6374db7e07fd9768b3038f
kimyovin <35512625+kimyovin@users.noreply.github.com>
2021-04-10 14:26:31 +0900

    Create 신규 아이디 추천_72410.py

---
+++ Programmers/신규 아이디 추천_72410.py
@@ -0,0 +1,38 @@
+import re
+def solution(new_id):
+    new_id = new_id.lower()
+    new_id=re.sub('[^a-z0-9-_.]', '', new_id)
+    new_id=re.sub('\.{2,}', '.', new_id)
+    new_id=re.sub('^\.|\.$', '', new_id)
+    if new_id == '':
+        new_id = 'a'
+    if len(new_id) >= 16:
+        new_id = new_id[:15]
+    new_id=re.sub('^\.|\.$', '', new_id)
+    
+    while len(new_id)<=2:
+        new_id += new_id[-1]
+
+    return new_id
+
+print(solution("...!@BaT#*..y.a-b_cdefghijklm."))
+
+#-------------------------------
+#with other solution
+
+from re import sub
+def solution1(new_id):
+    new_id = new_id.lower()
+    new_id = sub("[^a-z0-9-_.]", "", new_id)
+    new_id = sub("\.+", ".", new_id)
+    new_id = sub("(^\.|\.$)", "", new_id)
+    new_id = new_id if new_id else "a"
+    new_id = sub("\.$", "", new_id[:15])
+    new_id = new_id if len(new_id) > 3 else new_id + new_id[-1] * (3 - len(new_id))
+    return new_id
+
+'''
+1. if new_id => new_id가 ''이면 True
+
+https://programmers.co.kr/learn/courses/30/lessons/72410
+'''
