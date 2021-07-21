0e7802c416eae554deebd1b0dfc52afabf0164c2
kimyovin <35512625+kimyovin@users.noreply.github.com>
2021-07-21 15:54:02 +0900

    Create 땅따먹기_12913.py

---
+++ Programmers/땅따먹기_12913.py
@@ -0,0 +1,24 @@
+def solution(land):
+    answer = []
+    for i in range(0, len(land)-1):
+        land[i+1][0] += max(land[i][1],land[i][2],land[i][3])
+        land[i+1][1] += max(land[i][0],land[i][2],land[i][3])
+        land[i+1][2] += max(land[i][0],land[i][1],land[i][3])
+        land[i+1][3] += max(land[i][0],land[i][1],land[i][2])
+        
+    return max(land[-1])
+  
+#############################
+def solution2(land):
+
+    for i in range(1, len(land)):
+        for j in range(len(land[0])):
+            land[i][j] = max(land[i -1][: j] + land[i - 1][j + 1:]) + land[i][j]
+
+    return max(land[-1])
+  
+ 
+'''
+! DP 문제 !
+https://programmers.co.kr/learn/courses/30/lessons/12913
+'''
