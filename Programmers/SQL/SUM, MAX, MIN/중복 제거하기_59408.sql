8a870a88b6c70887504e6b2a1396c4ae589e1d18
kimyovin <35512625+kimyovin@users.noreply.github.com>
2021-08-28 23:51:29 +0900

    Update 중복 제거하기_59408.sql

--- Programmers/SQL/SUM, MAX, MIN/중복 제거하기_59408.sql
+++ Programmers/SQL/SUM, MAX, MIN/중복 제거하기_59408.sql
@@ -2,4 +2,10 @@ SELECT count(DISTINCT NAME) FROM ANIMAL_INS
 WHERE NAME is not NULL;
 
 # distinct (컬럼명)
+# 
+# count([ALL|DISTINCT] expression)
+# ALL : NULL을 제외한 모든 값
+# DISTINCT : NULL을 제외한 유니크한 값
+# * : NULL을 포함한 테이블의 모든 행의 개수
+
 # https://programmers.co.kr/learn/courses/30/lessons/59408
