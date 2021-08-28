862880cc6f6c36e1ff0309d02537c7f006e582fb
kimyovin <35512625+kimyovin@users.noreply.github.com>
2021-08-28 23:33:32 +0900

    Update 최솟값 구하기_59038.sql

--- Programmers/SQL/SUM, MAX, MIN/최솟값 구하기_59038.sql
+++ Programmers/SQL/SUM, MAX, MIN/최솟값 구하기_59038.sql
@@ -1,3 +1,6 @@
 SELECT MIN(DATETIME) FROM ANIMAL_INS;
+-----------------------------------
+# 같은 것
+SELECT DATETIME from ANIMAL_INS order by DATETIME limit 1;
 
 # https://programmers.co.kr/learn/courses/30/lessons/59038
