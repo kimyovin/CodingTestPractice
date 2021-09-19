b99d2e3bf29e3c49dfcf72bc8fc20042b9419359
kimyovin <35512625+kimyovin@users.noreply.github.com>
2021-09-19 18:53:03 +0900

    Create 숨바꼭질2.cpp

---
+++ Baekjoon/그래프 탐색/숨바꼭질2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+
+#define MAX 100001
+using namespace std;
+
+int N, K;   // N: 수빈이의 위치, K: 동생의 위치
+bool visited[MAX]={false,};
+int min_num=MAX;  // 최소 횟수
+int route_num=0;    // 방법 개수
+
+void bfs(int N, int K){
+    queue<pair<int, int>> que;
+    visited[N] = true;
+    que.emplace(make_pair(N, 0));
+    
+    while(!que.empty()){
+        int now = que.front().first;
+        int num = que.front().second;
+        que.pop();
+        
+        if(now == K){
+            min_num = num;
+            route_num ++;
+        }
+        if (num > min_num) {
+            return;
+        }
+        visited[now] = true;
+        
+        if(0<=now-1 && !visited[now-1])
+            que.emplace(make_pair(now-1, num+1));
+        if(now+1 <= K && !visited[now+1])
+            que.emplace(make_pair(now+1, num+1));
+        if(now*2 <= K+1 && !visited[now*2])
+            que.emplace(make_pair(now*2, num+1));
+        
+    }
+    return;
+}
+
+int main(int argc, const char * argv[]) {
+    // cin,cout 속도향상
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    cin >> N >> K;
+    bfs(N, K);
+    printf("%d\n%d\n", min_num, route_num);
+    
+    return 0;
+}
+
+// https://www.acmicpc.net/problem/12851
