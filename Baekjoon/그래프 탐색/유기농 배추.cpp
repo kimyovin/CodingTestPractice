7ab075e5cc2acc0ad05bd1921f4527cee27eadd3
kimyovin <35512625+kimyovin@users.noreply.github.com>
2021-09-20 22:04:12 +0900

    Update 유기농 배추.cpp
    Add bfs solution

--- Baekjoon/그래프 탐색/유기농 배추.cpp
+++ Baekjoon/그래프 탐색/유기농 배추.cpp
@@ -59,4 +59,92 @@ int main(int argc, const char * argv[]) {
     return 0;
 }
 
+/********************************************
+*
+* 나의풀이2 - BFS로 구현
+*
+********************************************/
+
+#include <string.h>
+#include <iostream>
+#include <queue>
+
+#define MAX 50
+using namespace std;
+
+struct POS{
+    int X, Y;
+};
+int T, M, N, K;
+int map[MAX][MAX];
+
+void bfs(int x_start, int y_start){
+    queue<POS> que;
+    que.push({x_start, y_start});
+    
+    map[x_start][y_start] = 2;   // (x, y)를 방문했다는 표시
+    while(!que.empty()){
+        int x = que.front().X;
+        int y = que.front().Y;
+        que.pop();
+        
+        // 위로 이동
+        if (0<=y-1 && map[x][y-1]==1){
+            que.push({x, y-1});
+            map[x][y-1] = 2;
+        }
+        // 좌로 이동
+        if (0<=x-1 && map[x-1][y]==1){
+            que.push({x-1, y});
+            map[x-1][y] = 2;
+        }
+        // 우로 이동
+        if (y+1<N && map[x][y+1]==1){
+            que.push({x, y+1});
+            map[x][y+1] = 2;
+        }
+        // 아래로 이동
+        if (x+1<M && map[x+1][y]==1){
+            que.push({x+1, y});
+            map[x+1][y] = 2;
+        }
+    }
+    return ;
+}
+
+int main(int argc, const char * argv[]) {
+    // cin,cout 속도향상
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    
+    cin >> T;
+    
+    for(int i=0; i<T; i++){
+        cin>> M>> N>> K;
+        int num=0;    // 벌레 개수
+        
+        memset(map, 0, sizeof(map));
+        for(int i=0; i<K;i++){
+            int X, Y;
+            cin >> X>> Y;
+            map[X][Y]=1;
+        }
+        
+        for(int x=0;x<M;x++){
+            for(int y=0;y<N;y++){
+                if(map[x][y]==1){
+                    bfs(x, y);
+                    num++;
+                }
+            }
+        }
+        
+        cout << num<<endl;
+    }
+    
+    return 0;
+}
+
+
+
 // https://www.acmicpc.net/problem/1012
